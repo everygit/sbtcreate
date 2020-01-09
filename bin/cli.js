558d0573a6b55583e80c60d87c8aac8231b21845
everygit <sbt0198@163.com>
2020-01-09 16:50:59 +0800

    say hello

---
+++ bin/cli.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+/**
+ * Copyright (c) 2020-present, everygit
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+ // 
+
+
+console.log("hello world");
