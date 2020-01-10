df960e0b2c354d01f7815c33663809fc53737699
everygit <sbt0198@163.com>
2020-01-10 17:06:41 +0800

    webpack base

--- src/creates/webpack/index.js
+++ src/creates/webpack/index.js
@@ -1,14 +1,40 @@
 
 const fs = require('fs');
 const path = require('path');
+const { mkdirsSync } = require('@xiaoerr/io');
 
 var packageObj = {
-    name: "cli-wenjian",
-    version: '1.0.0'
+    "name": "www",
+    "version": "1.0.0",
+    "description": "",
+    "scripts": {
+        "test": "echo \"Error: no test specified\" && exit 1"
+    },
+    "author": "",
+    "license": "ISC",
+    "dependencies": {
+        "webpack": "^4.41.5",
+        "webpack-dev-server": "^3.10.1"
+    }
 }
+
+var config =
+    `const path = require('path');
+
+module.exports = {
+    entry:'./src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist')
+    }
+}`;
 
 module.exports = async function (options) {
     console.log("webpack")
     var p = path.resolve(options._create_path, 'package.json');
     fs.writeFileSync(p, JSON.stringify(packageObj, null, '\t'));
+
+    mkdirsSync(path.resolve(options._create_path, 'build_config'));
+
+    fs.writeFileSync(path.resolve(options._create_path, 'build_config', 'webpack.config.js'), config);
+
 }
